f64a98ec32e2f787fd2e675811e643848e96f904
Lucas Tsunaki <lucas.tsunaki@helmholtz-berlin.de>
2026-04-15 10:55:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quantum Vault: Secure Token Authentication Without Classical Side Information Benchmarked on IBMQ
+
+This repository contains the basic methods for implementing the quantum vault protocol with IBMQ from 'Quantum Vault: Secure Token Authentication Without Classical Side Information Benchmarked on IBMQ' by L. Tsunaki et al., available at: [arXiv:](https://arxiv.org/abs/). 
+
+A script for implementing the SWAP test can be found at [swap_test.py]().
+The IBMQ calibration reports from the day of the implementations 08.04.2026 can be found at [calibration_reports]().
+
+This application has dependencies on NumPy version 2.3.1, Qiskit version 2.1.0 and Qiskit IBM Runtime version 0.45.0.
+Due to Qiskit's fast development, the code is not guaranteed to work with newer versions as the ones specified above. 
+
+If you used the code provided here, please cite this repository with the data from [CITATION.cff]() and the accompanying paper [arXiv:](https://arxiv.org/abs/).
---
+++ swap_test.py
@@ -0,0 +1,89 @@
+"""
+This script implements a SWAP test for the verification of the quantum vault protocol,
+as discussed in "Quantum Vault: Secure Token Authentication Without Classical Side Information Benchmarked on IBMQ"
+by Lucas Tsunaki and Boris Naydenov. 
+"""
+
+import numpy as np
+
+from qiskit import QuantumCircuit
+from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
+service = QiskitRuntimeService(channel="ibm_quantum_platform",
+                                    token="", # your token and instance go here
+                                    instance="")
+
+def qtvault_circuit_diff(
+        theta1 : float,
+        phi1 : float,
+        theta2 : float,
+        phi2 : float,
+        N : int
+):
+    """
+    Generates quantum vault circuit
+
+    Parameters
+    ----------
+    theta1 : float
+        Theta angle on Bloch shhere for first token state
+    phi1 : float
+        Phi angle on Bloch sphere for first token state
+    theta2 : float
+        Theta angle on Bloch shhere for second token state
+    phi2 : float
+        Phi angle on Bloch sphere for second token state
+    N : int
+        Number of swap-test rounds
+    """
+
+    circuit = QuantumCircuit(3, N)
+
+    circuit.r(theta=theta1, phi=phi1, qubit=0)
+    circuit.r(theta=theta2, phi=phi2, qubit=1)
+
+    # repeats N times the measurement of the SWAP test
+    for idx_N in range(N):
+        circuit.barrier()
+        circuit.reset(2)
+        circuit.h(2)
+        circuit.cswap(2, 0, 1)
+        circuit.h(2)
+        circuit.measure(2, idx_N)
+
+    return circuit
+
+real_backend = service.backend('ibm_marrakesh')
+
+# light optmization without gate approximation and gate cancelation
+pass_manager = generate_preset_pass_manager(optimization_level=0,
+                                            backend=real_backend,
+                                            layout_method='dense',
+                                            translation_method="synthesis",
+                                            approximation_degree=1)
+
+# other parameters
+sampler = SamplerV2(real_backend)
+N=20
+shots=1000
+
+# input the desired angles here
+theta1 = np.arccos(np.random.uniform(-1,1))
+theta2 = np.arccos(np.random.uniform(-1,1))
+phi1 = np.random.uniform(0,2*np.pi)
+phi2 = np.random.uniform(0,2*np.pi)
+
+# generates, transpiles and runs circuit
+circuit = qtvault_circuit_diff(
+    theta1,
+    phi1,
+    theta2,
+    phi2,
+    N
+    )
+
+transpiled_qc = pass_manager.run(circuit)
+job = sampler.run(transpiled_qc, shots=shots)
+
+print(job.job_id(), real_backend)
